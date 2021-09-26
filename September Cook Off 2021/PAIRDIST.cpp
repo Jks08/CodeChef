71584716395f335dc3e1a6de4ef9a6915b3cfa9d
Jishnu Srivastava <82699906+Jks08@users.noreply.github.com>
2021-09-26 11:17:41 +0530

    Create PAIRDIST.cpp
    My solution to Chef and Pairwise Distances question.

---
+++ September Cook Off 2021/PAIRDIST.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ll long long 
+#define vll vector<long long> 
+#define f(i,s,e) for(long long int i=s;i<e;i++) 
+
+void solve(){ 
+    ll n; 
+    cin>>n; 
+     vll arr(n); 
+    f(i,0,n)cin>>arr[i]; 
+    if(n>4){ 
+        cout<<"NO"<<endl; 
+        return; 
+    } 
+    cout<<"YES"<<endl; 
+    f(i,0,n){ 
+        if(i%4==0) 
+            cout<<arr[i]<<" 0\n"; 
+        else if(i%4==1) 
+            cout<<"0 "<<arr[i]<<endl; 
+        else if(i%4==2) 
+            cout<<-arr[i]<<" 0\n"; 
+        else 
+            cout<<"0 "<<-arr[i]<<endl; 
+    } 
+} 
+int main() { 
+    ll tc=1; 
+    cin>>tc; 
+    while(tc--){ 
+        solve(); 
+    } 
+    return 0; 
+}
