1c40414410c1fb504f243af87ba415009eb2f047
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2021-10-19 21:12:17 +0530

    Create MAXDISTKT.cpp

---
+++ SnackDown 2021 Qualifier/MAXDISTKT.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+bool cmp(pair<int,int> &a,pair<int,int> &b){
+    return a.second < b.second;
+}
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;cin>>n;
+	    vector<pair<int,int>> arr;
+	    for(int i=0;i<n;i++){
+	        int x;
+	        cin>>x;
+	        arr.push_back({x,i});
+	    }
+	    sort(arr.begin(),arr.end());
+	    int k=0;
+	    for(int i=0;i<n;i++){
+	        if(arr[i].first>k){
+	            arr[i].first=k;
+	            k++;
+	        }
+	    }
+	    sort(arr.begin(),arr.end(),cmp);
+	    for(int i=0;i<n;i++){
+	        cout<<arr[i].first<<" ";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
