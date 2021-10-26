9e82b95738a071fabee1d36788f4c326ea1b0eee
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2021-10-26 19:00:11 +0530

    Create SECPLAYER.cpp

---
+++ October CookOff 2021/SECPLAYER.cpp
@@ -0,0 +1,181 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+typedef vector<int> vi;
+#define f first
+#define s second
+#define derr if(0) cerr
+
+
+template<class Fun>
+class y_combinator_result {
+  Fun fun_;
+public:
+  template<class T>
+  explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+
+  template<class ...Args>
+  decltype(auto) operator()(Args &&...args) {
+    return fun_(std::ref(*this), std::forward<Args>(args)...);
+  }
+};
+
+template<class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+  return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<vector<ll>> matrix;
+
+template <int MOD_> struct modnum {
+  static constexpr int MOD = MOD_;
+  static_assert(MOD_ > 0, "MOD must be positive");
+
+private:
+  using ll = long long;
+
+  int v;
+
+  static int minv(int a, int m) {
+    a %= m;
+    assert(a);
+    return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
+  }
+
+public:
+
+  modnum() : v(0) {}
+  modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+  explicit operator int() const { return v; }
+  friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
+  friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+
+  friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+  friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+
+  modnum inv() const {
+    modnum res;
+    res.v = minv(v, MOD);
+    return res;
+  }
+  modnum neg() const {
+    modnum res;
+    res.v = v ? MOD-v : 0;
+    return res;
+  }
+
+  modnum operator- () const {
+    return neg();
+  }
+  modnum operator+ () const {
+    return modnum(*this);
+  }
+
+  modnum& operator ++ () {
+    v ++;
+    if (v == MOD) v = 0;
+    return *this;
+  }
+  modnum& operator -- () {
+    if (v == 0) v = MOD;
+    v --;
+    return *this;
+  }
+  modnum& operator += (const modnum& o) {
+    v += o.v;
+    if (v >= MOD) v -= MOD;
+    return *this;
+  }
+  modnum& operator -= (const modnum& o) {
+    v -= o.v;
+    if (v < 0) v += MOD;
+    return *this;
+  }
+  modnum& operator *= (const modnum& o) {
+    v = int(ll(v) * ll(o.v) % MOD);
+    return *this;
+  }
+  modnum& operator /= (const modnum& o) {
+    return *this *= o.inv();
+  }
+
+  friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
+  friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
+  friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+  friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+  friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+  friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
+};
+
+template <typename T> T pow(T a, long long b) {
+  assert(b >= 0);
+  T r = 1; while (b) { if (b & 1) r *= a; b >>= 1; a *= a; } return r;
+}
+typedef modnum<1000000007> mnum;
+
+void rsolve() {
+  int n;
+  cin >> n;
+  vector<pii> v(n);
+  for(int i = 0; i < n; i++) {
+    cin >> v[i].f;
+    v[i].s = i;
+  }
+  sort(all(v));
+  mnum base = 2;
+  mnum scale = 5;
+  mnum inc = 4;
+  for(int i = 4; i <= n; i++) {
+    base *= scale;
+    scale += inc++;
+  }
+  mnum nn = 2;
+  mnum dd = 4;
+  mnum ret = 0;
+  mnum amt = 0;
+  for(int i = n-2; i >= 0; i--) {
+    ret += v[i].f * base;
+    amt += base;
+    base *= nn++;
+    base /= dd++;
+  }
+  cout << ret/amt << "\n";
+}
+void solve() {
+  int t;
+  cin >> t;
+  while(t--) rsolve();
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  solve();
+}
